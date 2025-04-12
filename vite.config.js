88ee7fec07e0db29459cceb6647351acc46e7437
peritissimus <149.kush@gmail.com>
2025-04-13 05:02:53 +0530

    chore(deps): remove PWA plugin and update dependency versions
    Updated the `package.json` to remove the `vite-plugin-pwa` dependency and added `vite-plugin-compression` and `terser` to the development dependencies. Adjusted other related dependencies in `package-lock.json`.
    
    Also, removed PWA related code from `vite.config.js` to streamline configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,77 +1,8 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import compress from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Kushal - Fullstack Developer & Designer',
-        short_name: 'Kushal',
-        description: 'Portfolio of Kushal, a fullstack developer and designer',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'gstatic-fonts-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-          {
-            urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdnjs-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 days
-              },
-              cacheableResponse: {
-                statuses: [0, 200],
-              },
-            },
-          },
-        ],
-      },
-    }),
     compress({
       algorithm: 'brotliCompress',
       ext: '.br',
