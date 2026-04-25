c12fcc19aff7654823392690c3b1614d7a1c7973
peritissimus <149.kush@gmail.com>
2026-04-26 00:34:27 +0530

    refactor(src/content.config): update content config to use glob loader

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/blog' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -16,7 +17,7 @@ const blogCollection = defineCollection({
 });
 
 const workCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/work' }),
   schema: z.object({
     title: z.string(),
     company: z.string(),
